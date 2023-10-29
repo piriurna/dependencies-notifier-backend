c5caae82ad7701190249046fe41259fe87102c1f
Franco <francozalamena@gmail.com>
2023-10-29 16:01:06 +0000

    Removed addition of projects on register

--- src/routes/users.js
+++ src/routes/users.js
@@ -5,38 +5,14 @@ const router = express.Router();
 
 // Register or update a user's dependencies and FCM token
 router.post('/register', async (req, res) => {
-  const { fcmToken, dependencies, userId } = req.body;
-
-  // Let's process each dependency
-  const processedDeps = [];
-  for (let dep of dependencies) {
-      // Find if this dependency already exists
-      let dependency = await Dependency.findOne({ group: dep.group, name: dep.name });
-
-      if (!dependency) {
-          // If it doesn't exist, create a new one
-          dependency = new Dependency({ 
-              group: dep.group,
-              name: dep.name,
-              latestVersion: dep.version // or whatever default you want 
-          });
-          await dependency.save();
-      }
-
-      // Add this dependency to the processed list
-      processedDeps.push({
-          dependency: dependency._id, 
-          currentVersion: dep.version
-      });
-  }
+  const { fcmToken, userId } = req.body;
 
   // Store or update the user's data in the database
   let user = await User.findOne({ userId });
   if (user) {
-      user.dependencies = processedDeps;
       await user.save();
   } else {
-      user = new User({ userId, fcmToken, dependencies: processedDeps });
+      user = new User({ userId, fcmToken });
       await user.save();
   }
 
