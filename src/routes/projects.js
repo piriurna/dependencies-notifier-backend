6e6ec1367b58565cbfba02a41b2ebf908603520c
Franco <francozalamena@gmail.com>
2023-11-02 16:12:20 +0000

    Project response with only te data

--- src/routes/projects.js
+++ src/routes/projects.js
@@ -32,7 +32,7 @@ router.get('/:projectId', async (req, res) => {
     try {
         const project = await Project.findById(req.params.projectId).populate('dependencies.dependency');
         if (project) {
-            res.status(200).json({project});
+            res.status(200).json(project);
         } else {
             res.status(404).send({ error: 'Project not found.' });
         }
